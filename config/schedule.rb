72dca9c81bc24609b80a6700b7e97dd554ef7ae1
Eric O <eric@ejdigitalmedia.com>
2019-02-09 15:32:28 -0500

    Add mailifrc to Whenever gem cron runs

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,6 +10,13 @@ set :environment, Rails.env
 # Log cron output to app log directory
 set :output, Rails.root.join("log/#{Rails.env}_cron_log.log")
 
+# Our job template wraps the cron job in a script that emails out any errors.
+# This is a CUL provided script. More details can be found here:
+# https://wiki.library.columbia.edu/display/USGSERVICES/Cron+Management
+set :email_subject, 'Hysync Cron Error (via Whenever Gem)'
+set :error_recipient, Rails.application.credentials[:error_email_recipient]
+set :job_template, "/usr/local/bin/mailifrc -s 'Error - :email_subject' :error_recipient -- /bin/bash -l -c ':job'"
+
 # Override default rake task job type
 job_type :rake, "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
