bc348688e7ddaae77df4d58b515953a50ecc34ed
Eric O <eric@ejdigitalmedia.com>
2021-06-08 19:31:58 -0400

    Update series syncing rules for ldeotechnical mapping

--- lib/hysync/marc_synchronizer/marc_parsing_methods/series.rb
+++ lib/hysync/marc_synchronizer/marc_parsing_methods/series.rb
@@ -27,16 +27,26 @@ module Hysync
             end
           end
 
-          return series_values if series_values.present?
-
-          # If no series was found, return values from backup field 490
-          MarcSelector.all(marc_record, 490, indicator1: 0, a: true).map do |field|
-            {
-              'series_title' => StringCleaner.trailing_punctuation_and_whitespace(field['a'])
-            }.tap do |series|
-              series['series_is_columbia'] = true if mapping_ruleset == 'ldeotechnical'
+          if series_values.blank?
+            # If no 830 series was found, return values from backup field 490
+            series_values = MarcSelector.all(marc_record, 490, indicator1: 0, a: true).map do |field|
+              {
+                'series_title' => StringCleaner.trailing_punctuation_and_whitespace(field['a'])
+              }.tap do |series|
+                series['series_is_columbia'] = true if mapping_ruleset == 'ldeotechnical'
+              end
             end
           end
+
+          if mapping_ruleset == 'ldeotechnical'
+            # Always add extra local series 'LDEO Technical Reports'
+            series_values << {
+              'series_title' => 'LDEO Technical Reports',
+              'series_is_columbia' => true
+            }
+          end
+
+          series_values
         end
       end
     end
