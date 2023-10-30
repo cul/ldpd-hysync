6393994567672ebff5dfafc0c15c931a0a883702
Eric O <eric@ejdigitalmedia.com>
2023-10-30 17:53:48 -0400

    Deployment config updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,22 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :department, 'ldpd'
-set :instance, fetch(:department)
+set :remote_user, "renserv"
 set :application, 'hysync'
-set :repo_name, "#{fetch(:department)}-#{fetch(:application)}"
+set :repo_name, "ldpd-#{fetch(:application)}"
+set :repo_url, "git@github.com:cul/#{fetch(:repo_name)}.git"
 set :deploy_name, "#{fetch(:application)}_#{fetch(:stage)}"
+
 # used to run rake db:migrate, etc
 # Default value for :rails_env is fetch(:stage)
 set :rails_env, fetch(:deploy_name)
+
 # use the rvm wrapper
 set :rvm_ruby_version, fetch(:deploy_name)
 
-set :repo_url, "git@github.com:cul/#{fetch(:repo_name)}.git"
-
-set :remote_user, "#{fetch(:instance)}serv"
 # Default deploy_to directory is /var/www/:application
 # set :deploy_to, '/var/www/my_app_name'
-set :deploy_to, "/opt/passenger/#{fetch(:instance)}/#{fetch(:deploy_name)}"
+set :deploy_to, "/opt/passenger/#{fetch(:deploy_name)}"
 
 # Default value for :scm is :git
 # set :scm, :git
